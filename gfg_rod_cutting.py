350de649fa5f1eda86dbf54af706b5dadb8bbb28
Omkar-23 <omkarvya63@gmail.com>
2024-03-28 19:16:39 +0530

    variation of unbounded kp

---
+++ gfg_rod_cutting.py
@@ -0,0 +1,18 @@
+def rod_cut(price, len_rod, n, le):
+    t = [[0 for i in range(n+1)] for i in range(le+1)]
+    for i in range(le+1):
+        for j in range(n+1):
+            if i ==0 or j ==0:
+                t[i][j] =0
+            elif(len_rod[i-1]<=j):
+                t[i][j] = max(price[i-1]+t[i][j-len_rod[i-1]],t[i-1][j])
+            else:
+                t[i][j] = t[i-1][j]
+    return t[le][n]
+
+if __name__ == '__main__':
+    n = 8
+    price =[3, 5, 8, 9, 10, 17, 17, 20]
+    le = len(price)
+    len_rod = [i for i in range(1,le+1)]
+    print(rod_cut(price, len_rod, n, le))
